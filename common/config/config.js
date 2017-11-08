5cd88f050029de01876a695cb2daf1a750abda69
Yibay <514531536@qq.com>
2017-11-08 13:46:47 +0800

    调整search页路由配置

--- common/config/config.js
+++ common/config/config.js
@@ -1,5 +1,7 @@
 // 基础url地址
 var base_url = '/Web-netease-Ego';
+// 页面路由
+var page_url = '/Web-netease-Ego/html';
 // api 地址
 // var api_url = 'http://59.111.99.234';
 var api_url = '/Web-netease-Ego/mock.data/interface'
